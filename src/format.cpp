d09f03cb200093c94e9cbd753802cd03370a2d2e
pdagger <pf_gr@hotmail.com>
2019-08-30 11:31:23 -0400

    refact: Added the option to show days in ElapsedTime()

--- src/format.cpp
+++ src/format.cpp
@@ -9,12 +9,24 @@ using std::to_string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 string Format::ElapsedTime(long seconds) { 
-	int hh, remaining_s, mm, ss;
+	int dd, hh, remaining_s, mm, ss;
 
-	hh = seconds / 3600;
-	remaining_s = seconds % 3600;
+	// Get days
+	dd = seconds / 86400;
+	remaining_s = seconds % 86400;
+	// Get hours
+	hh = remaining_s / 3600;
+	remaining_s = remaining_s % 3600;
+	// Get minutes
 	mm = remaining_s / 60;
+	// Get total remaining seconds
 	ss = remaining_s % 60;
 
-	return to_string(hh) + ":" + to_string(mm) + ":" + to_string(ss);
+	if (dd == 0) {
+		return to_string(hh) + ":" + to_string(mm) + ":" + to_string(ss);
+	}
+	else{
+		return to_string(dd) + " days " + to_string(hh) + ":" + to_string(mm) + ":" + to_string(ss);
+	}
+	
 }
